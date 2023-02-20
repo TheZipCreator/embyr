a30a6f7a6362e7dee0cb21f6d9fc5fc056f59dbf
TheZipCreator <thezipcreator@protonmail.com>
2023-02-20 16:59:31 -0500

    Updated version number

--- source/app.d
+++ source/app.d
@@ -4,7 +4,7 @@ import std.stdio, std.getopt, std.file, std.json, std.base64, std.zlib, std.form
 
 import peg, compiler, declaration;
 
-enum VERSION = "0.2.3";
+enum VERSION = "0.3.0";
 
 int main(string[] args) {
 	enum Format {
